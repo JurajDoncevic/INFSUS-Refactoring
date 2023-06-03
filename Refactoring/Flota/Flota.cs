c08a08a3e2126c20f0c9c05b1e5c2decf580295b
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2023-06-03 15:19:21 +0200

    Law of Demeter on `DohvatiNajblizeFLotinoPlovilo`; introduces lokacijaX and lokacijaY instead of entire Plovilo

--- Refactoring/Flota/Flota.cs
+++ Refactoring/Flota/Flota.cs
@@ -8,24 +8,24 @@ public class Flota
 
     public void ZoviUPomocNajblizeFlotinoPlovilo(Plovilo unesrecenoPlovilo)
     {
-        Plovilo najblizePlovilo = DohvatiNajblizeFlotinoPlovilo(unesrecenoPlovilo);
+        Plovilo najblizePlovilo = DohvatiNajblizeFlotinoPlovilo(unesrecenoPlovilo.LokacijaX, unesrecenoPlovilo.LokacijaY);
 
         // najblize plovilo flote neka provjeri lokaciju
         najblizePlovilo.ProvjeriSOSLokaciju(unesrecenoPlovilo.LokacijaX, unesrecenoPlovilo.LokacijaY);
     }
 
-    private Plovilo DohvatiNajblizeFlotinoPlovilo(Plovilo plovilo)
+    private Plovilo DohvatiNajblizeFlotinoPlovilo(double lokacijaX, double lokacijaY)
     {
         Plovilo najblizePlovilo = Plovila.First(); // uvijek ima plovila u Floti
-        double najblizaUdaljenost = najblizePlovilo.UdaljenostOd(plovilo.LokacijaX, plovilo.LokacijaY);
+        double najblizaUdaljenost = najblizePlovilo.UdaljenostOd(lokacijaX, lokacijaY);
 
         // pronadi najblize plovilo unesrecenom plovilu
         foreach (var flotinoPlovilo in Plovila)
         {
 
-            if (flotinoPlovilo.UdaljenostOd(plovilo.LokacijaX, plovilo.LokacijaY) < najblizaUdaljenost)
+            if (flotinoPlovilo.UdaljenostOd(lokacijaX, lokacijaY) < najblizaUdaljenost)
             {
-                najblizaUdaljenost = flotinoPlovilo.UdaljenostOd(plovilo.LokacijaX, plovilo.LokacijaY);
+                najblizaUdaljenost = flotinoPlovilo.UdaljenostOd(lokacijaX, lokacijaY);
                 najblizePlovilo = flotinoPlovilo;
             }
         }
